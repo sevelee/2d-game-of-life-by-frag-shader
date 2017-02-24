8b493365fd23ae76bd017b13c1433f1f98f07efe
sevelee <seve.lee@foxmail.com>
2017-02-24 23:33:00 +0900

    ver 0.0

---
+++ Assets/2DGameOfLife/Scripts/AddNewLife.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddNewLife : MonoBehaviour {
+
+    public Texture newLife;
+    public RenderTexture renderTexture;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+        {
+            RenderTexture.active = renderTexture;
+            int positionx, positiony;
+            positionx = (int)(Random.value * Screen.width);
+            positiony = (int)(Random.value * Screen.height);
+            Graphics.DrawTexture(new Rect(positionx, positiony, newLife.width, newLife.height), newLife);
+            RenderTexture.active = null;
+        }
+	}
+}
---
+++ Assets/2DGameOfLife/Scripts/WorldContorl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldContorl : MonoBehaviour {
+
+    //material with the CA shader
+    public Material material;
+    public RenderTexture renderTexture;
+    public Texture originalTexture;
+
+    public float updateTimeStep = 1.0f;
+
+    RenderTexture tempRenderTexture;
+    float timeSinceLastUpdate = 0;
+
+    void Init()
+    {
+        //copy the texture to render texture
+        Graphics.Blit(originalTexture, renderTexture);
+        tempRenderTexture = new RenderTexture(renderTexture.width, renderTexture.height, renderTexture.depth);
+    }
+
+	// Use this for initialization
+	void Start () {
+        Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeSinceLastUpdate += Time.deltaTime;
+		if (timeSinceLastUpdate > updateTimeStep)
+        {
+            timeSinceLastUpdate -= updateTimeStep;
+            updateCells();
+        }
+	}
+
+    private void updateCells()
+    {
+        Graphics.Blit(renderTexture, tempRenderTexture, material);
+        Graphics.Blit(tempRenderTexture, renderTexture);
+    }
+}
